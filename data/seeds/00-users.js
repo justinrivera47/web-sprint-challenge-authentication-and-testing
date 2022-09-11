2c6b48abe4ebd546ae892615d105aad291d8f529
justinrivera47 <justinrivera47@gmail.com>
2022-09-11 18:11:44 -0400

    I think the id test passes now

--- data/seeds/00-users.js
+++ data/seeds/00-users.js
@@ -2,6 +2,6 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').truncate()
   await knex('users').insert([
-    {username: 'Captain Marvel', password: '$2a$06$BUFPouZvJN6z.EnotjgrWeX7Ipj9rlwQPMtRy83t1ZVavXE6ZgNuq'} //foobar
+    {id: 1, username: 'Captain Marvel', password: '$2a$06$BUFPouZvJN6z.EnotjgrWeX7Ipj9rlwQPMtRy83t1ZVavXE6ZgNuq'} //foobar
   ]);
 };
