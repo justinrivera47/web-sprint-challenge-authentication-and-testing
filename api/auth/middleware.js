69b1cab70f85f40ed84bfbcc54b308d4ded8a4d5
justinrivera47 <justinrivera47@gmail.com>
2022-09-12 13:50:37 -0400

    changed null to undefined

--- api/auth/middleware.js
+++ api/auth/middleware.js
@@ -2,7 +2,9 @@ const User = require('./model')
 
 function validateUser(req, res, next) {
   let { username, password } = req.body
-  if(typeof username != 'string' || username.trim() == '' || password.trim() == '' || password == null || username == null) {
+  console.log(username, password)
+  
+  if(typeof username != 'string' || username.trim() == '' || password.trim() == '' || password == undefined || username == undefined) {
     res.status(404).json({ message: "username and password required"});
     return
   } else {
